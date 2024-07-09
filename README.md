cd2fb5bf125bfb0a431209fa4dcf729b2fac2a7a
Oleg <oleg-tsv@yandex.ru>
2024-07-10 00:48:58 +0300

    new version release

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 ![](https://img.shields.io/badge/cloudflare%20pages-F38020?style=for-the-badge&logo=cloudflare&logoColor=white)
 [![Licence](https://img.shields.io/github/license/Copysiper/copysiper.github.io?style=for-the-badge)](./LICENSE)
 
+[![Node.js CI](https://github.com/Hekzory/newPersonalPage/actions/workflows/node.js.yml/badge.svg)](https://github.com/Hekzory/newPersonalPage/actions/workflows/node.js.yml)
+
 <h1 align="center">
 GitHub-репозиторий сайта-визитки
 </h1>
 
 <p align="center">
-<img height="400" src="https://repository-images.githubusercontent.com/386972042/4d776f21-15c5-44f8-9b80-60f6cbf4900b" alt="banner">
+<img height="400" src="https://tsv.one/preview.png" alt="banner">
 </p>
 
 <p align="center">
---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { compression } from "vite-plugin-compression2";
+import { createHtmlPlugin } from "vite-plugin-html";
+import htmlMetaPlugin from "./vite-plugin-html-meta";
+import constants from "node:zlib";
+import fs from "fs";
+
+export default defineConfig({
+    appType: "mpa",
+    plugins: [
+        htmlMetaPlugin(),
+        createHtmlPlugin({
+            minify: true,
+        }),
+        compression({
+            algorithm: "brotliCompress",
+            exclude: [/\.(br)$/, /\.(gz)$/],
+            compressionOptions: {
+                level: 9,
+            },
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                notfoundpage: resolve(__dirname, "404.html"),
+            },
+        },
+        target: "es2021",
+        minify: "terser",
+        cssMinify: "lightningcss",
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+                passes: 2,
+                ecma: 2021,
+                module: true,
+                toplevel: true,
+            },
+            mangle: {
+                toplevel: true,
+                module: true,
+            },
+            format: {
+                comments: false,
+                ecma: 2021,
+            },
+        },
+    },
+});
